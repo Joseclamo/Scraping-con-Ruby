a8473ea852ed7cf835b698c9269fdf80de6e85b7
Jose <joseclamo@gmail.com>
2018-04-06 23:10:25 +0200

    translation

--- scrap-en.rb
+++ scrap-en.rb
@@ -5,5 +5,5 @@ html = open("https://etherscan.io/").read
 doc = Nokogiri::HTML(html)
 
 doc.xpath('.//*[@id="ContentPlaceHolder1_Label1"]/a/font').map do |element|
-  puts "El último bloque de la red de Ethereum es el: #{element.inner_text}"
+  puts "The last block in the Ethereum network is: #{element.inner_text}"
 end
